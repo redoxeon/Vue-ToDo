aec567fc28042c15738b638eefd2b797d5d8b64e
Michael Harding <redoxeon@mharding.dev>
2024-05-01 10:48:38 -0600

    init

---
+++ src/components/__tests__/HelloWorld.spec.js
@@ -0,0 +1,12 @@
+// Auto-generated. Will delete, but going to use as a reference.
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import HelloWorld from '../HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
---
+++ src/stores/counter.js
@@ -0,0 +1,13 @@
+// Auto-generated. Will delete, but going to use as a reference.
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, increment }
+})
